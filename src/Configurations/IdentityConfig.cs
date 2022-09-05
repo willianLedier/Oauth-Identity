831225e507ec4820084af48e775ab0b7cd975e2c
Willian Ledier <willian.almeida@socialbank.com.br>
2022-09-05 17:09:07 -0300

    src

---
+++ src/Configurations/IdentityConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Oauth_Identity.Data;
+
+
+namespace Oauth_Identity.Configurations
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddJwtConfiguration(configuration);
+
+            return services;
+        }
+    }
+}
---
+++ src/Models/ResponseMessage.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using Oauth_Identity.Messages;
+
+namespace Oauth_Identity.Integration
+{
+    public class ResponseMessage : Message
+    {
+        public ValidationResult ValidationResult { get; set; }
+
+        public ResponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+    }
+}
